a8d28cf02092e58de181d359323889ac727c0f6e
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-11-04 13:41:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotHackers::Application.config.secret_token = '0cd4c0d87b4972b7790f36f111d1b410f8cada9b5633f58f3f6a4b20eee9f4fb187affe900347c84ab2f984d1af9b48de8f0ca3d7cd1f3b931e40f02fa434aed'
