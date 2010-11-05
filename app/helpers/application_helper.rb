26c50fa06cb01d0ed1c27950061578d0588f8d45
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-11-05 10:06:58 +0100

    Add gravatars, strip usernames, link table names to GitHub.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  GRAVATAR_BASE_URL = 'http://www.gravatar.com/avatar'
+  def gravatar_url(gravatar_id, options)
+    options = options.reverse_merge('d' => 'mm')
+    query_string = options.to_query
+    "#{GRAVATAR_BASE_URL}/#{gravatar_id}?#{query_string}"
+  end
+
+  GITHUB_BASE_URL = 'https://github.com'
+  def github_url(github_username)
+    "#{GITHUB_BASE_URL}/#{github_username}"
+  end
 end
--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -1,11 +1,17 @@
 class Hacker < ActiveRecord::Base
   attr_accessible :username
 
+  before_validation :strip_username
+
   validates :username, :presence => true,
                        :uniqueness => true
   validate :username_is_github_user, :on => :create
 
   protected
+  
+  def strip_username
+    self.username = self.username.strip
+  end
 
   GITHUB_BASE_URL = 'http://github.com/api/v2/yaml/user/show/'
 
