7122bd2ab1dc75c57b888ce3ff069ca6068abc1e
Jorge <jorge@mrdias.com>
2010-11-05 17:38:25 +0100

    Changes to highlight first positions

--- app/helpers/hackers_helper.rb
+++ app/helpers/hackers_helper.rb
@@ -1,2 +1,10 @@
 module HackersHelper
+  def class_for_hacker_row(position)
+    case position
+    when 1 then "first"
+    when 2 then "second"
+    when 3 then "third"
+    else        ""
+    end
+  end
 end
