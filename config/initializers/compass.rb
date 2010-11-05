7683723ea4e2e548176fd0f4909e4b757fa03123
Jorge <jorge@mrdias.com>
2010-11-05 10:32:11 +0100

    Changes for deploy

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,7 +1,9 @@
-require 'compass'
-require 'fancy-buttons'
-# If you have any compass plugins, require them here.
+if Rails.env.development?
+  require 'compass'
+  require 'fancy-buttons'
+  # If you have any compass plugins, require them here.
 
-Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
-Compass.configure_sass_plugin!
+  Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.config"))
+  Compass.configuration.environment = RAILS_ENV.to_sym
+  Compass.configure_sass_plugin!
+end
